50211cff2a3bfb6ff48ffa93439010247ad9805c
wesleyem@mba <wesm.17@gmail.com>
2017-11-05 14:31:44 -0500

    cleaned up some things

--- content_scripts/tpb.js
+++ content_scripts/tpb.js
@@ -3,21 +3,35 @@ Author: wesleyem
 Project: tpbjs
 */
 
+// Selector for the table
 var table = "#searchResult"
+
 // Selector for untrusted and non-vip users in the 'single' view
 var hideableS = "nobr a:only-of-type"
 
 // Selector for untrusted and non-vip users in the 'double' view
 var hideableD = "td a:only-of-type:not([class])"
 
+// Selector for the viewswitch
+var viewswitch = "div.viewswitch a"
+
 if ($(table).length > 0)
 {
-	if ($("div.viewswitch a").attr("href").includes("view=s"))
+	if ($(viewswitch).attr("href").includes("view=s"))
 	{
-		var x = $(hideableD).closest("tr").fadeTo("fast", 0.2, function(){})
+		fadeUnwanted(hideableD)
 	}
 	else
 	{
-		var y = $(hideableS).closest("tr").fadeTo("fast", 0.2, function(){})
+		fadeUnwanted(hideableS)
+	}
+}
+
+function fadeUnwanted(selector) {
+	var emptycallback = function (){}
+	if (selector) {
+		$(selector).closest("tr").fadeTo("fast", 0.2, emptycallback)
+	} else {
+		console.log("No selector given")
 	}
 }
